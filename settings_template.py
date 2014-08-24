4837e5b3ebb08afc9088dcda74ea3fe5160e71fc
wumaogit <actor2019@gmail.com>
2014-08-24 18:33:35 +0900

    init

---
+++ settings_template.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# use (id,key) or X-AVOSCloud-Request-Sign to auth
+# https://cn.avoscloud.com/docs/rest_api.html
+avos_app_id = ''
+avos_app_key = ''
+avos_app_master_key = ''
